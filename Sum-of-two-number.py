01d9c28c52a977706356adb0fd30361ed3b4febc
Vivek <vivekgarg.vg331@gmail.com>
2024-09-10 23:10:07 +0530

    Sum of two number python code

---
+++ Sum-of-two-number.py
@@ -0,0 +1,13 @@
+
+def sum_of_2_number(num1 , num2):
+    sum_result = num1+num2
+    return sum_result
+
+num1 = int(input("Enter your first no.: "))
+num2 = int(input("Enter your second no.: ")) 
+
+result = sum_of_2_number(num1 , num2)
+
+print("sum of 2 number is: " , result)
+
+
