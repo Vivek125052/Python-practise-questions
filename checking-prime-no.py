5c79f884c383be2f19782ba9c3bf341d75128a58
Vivek <vivekgarg.vg331@gmail.com>
2024-09-12 16:06:15 +0530

    checking the no. if it is prime or not

---
+++ checking-prime-no.py
@@ -0,0 +1,10 @@
+number = int(input("Enter the number you wnat to know if it is prime or not: "))
+if number == 1:
+    print("The number is not prime")
+elif number >1:
+    for i in range(2,number):
+        if(number%i == 0):
+            print("The number is not prime")
+            break
+        else:
+            print("the no. is prime")
