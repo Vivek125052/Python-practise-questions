7102ba53886cb1148c3a00fdb722c26eb48f636d
Vivek <vivekgarg.vg331@gmail.com>
2024-09-17 00:16:05 +0530

    Checking Armstrong number

---
+++ Armstrong.py
@@ -0,0 +1,15 @@
+number = int(input("Enter the number you want to check if the number is armstrong or not: "))
+
+sum = 0
+temp = number
+
+while temp >0:
+    digit = temp%10
+    cube = digit**3
+    sum = sum + cube
+    temp //=10
+
+if sum == number:
+    print("Yes it is armstrong number")
+else:
+    print("The number is not an armstrong number")
