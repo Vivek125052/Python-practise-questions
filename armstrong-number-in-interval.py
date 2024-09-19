d11df39ae58aa6fe5cbfe0d7a669a869ddbaae10
Vivek <vivekgarg.vg331@gmail.com>
2024-09-19 16:54:35 +0530

    Armstrong number in an given interval

---
+++ armstrong-number-in-interval.py
@@ -0,0 +1,19 @@
+lower = int(input("Enter lower limit: "))
+upper = int(input("Enter upper limit: "))
+
+
+for number in range(lower , upper+1):
+    temp = number
+    sum = 0
+    while temp>0:
+        order = len(str(number))
+        digit = temp %10
+        cube = digit **order
+        sum = cube + sum
+        temp //=10
+
+    if sum == number:
+        print(f"The armStorng number betweer{lower} and {upper} limit is {number}")
+    else:
+        ("No armstrong number")
+
