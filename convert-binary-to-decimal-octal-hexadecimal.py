1c35f1e3513d514002277f0b67c209f4b681cd06
Vivek <vivekgarg.vg331@gmail.com>
2024-09-30 22:54:57 +0530

    Conversion of decimal to binary,octal and hexadecimal

---
+++ convert-binary-to-decimal-octal-hexadecimal.py
@@ -0,0 +1,4 @@
+decimal = int(input("Enter the decimal numeber: "))
+print("The conversion of decimal to binary is: " , bin(decimal))
+print("The conversion of decimal to Octal is: " , oct(decimal))
+print("The conversion of decimal to hexadecimal is: " , hex(decimal))
